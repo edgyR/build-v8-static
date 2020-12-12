a861853dc5b41dc43e788392cf96a4f9faf2b1ae
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-12-11 20:35:59 -0800

    cleanup

--- bionic-amd64-host-arm64-target/build.sh
+++ bionic-amd64-host-arm64-target/build.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-docker build --tag v8-bionic-arm64:latest --file Dockerfile .
-docker run --name v8-bionic-arm64 v8-bionic-arm64:latest
-docker cp v8-bionic-arm64:/v8-8.3.110.13-static.tar.xz .
-tar tvf v8-8.3.110.13-static.tar.xz
+docker build --tag edgyr/v8-bionic-arm64:latest --file Dockerfile .
+echo 'Ignore "no such container" errors'
+docker rm --force v8-container || true
+docker run -it --name v8-container edgyr/v8-bionic-arm64:latest tar -tvf /v8-8.3.110.13-static.tar.xz
+docker cp v8-container:/v8-8.3.110.13-static.tar.xz .
